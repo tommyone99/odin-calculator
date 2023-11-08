7a85a18a364631e3b217f98da3e63ffd2f354e9f
Thomas One <tommyone99@gmail.com>
2023-11-07 19:33:41 -0500

    create new 'operate' function with conditional processes

--- script-calc.js
+++ script-calc.js
@@ -2,6 +2,22 @@ let firstNum = 0;
 let operator = "";
 let secondNum = 0;
 
+function operate(firstNum, operator, secondNum) {
+    let answer = 0;
+
+    if (operator == "+") {
+        answer = add(firstNum, secondNum);
+    }
+    else if (operator === "-") {
+        answer = subtract(firstNum, secondNum);
+    }
+    else if (operator === "*") {
+        answer = multiply(firstNum, secondNum);
+    }
+    else if (operator === "/") {
+        answer = divide(firstNum, secondNum);
+    };
+};
 
 function add(a, b) {
     let c = a + b;
