f4db427c56e1eaab0ea10a48e8d022a145605bfa
Thomas One <tommyone99@gmail.com>
2023-11-07 19:27:06 -0500

    initialize main 3 variables for display updates

--- script-calc.js
+++ script-calc.js
@@ -1,19 +1,24 @@
+let firstNum = 0;
+let operator = "";
+let secondNum = 0;
+
+
 function add(a, b) {
     let c = a + b;
     return c;
-}
+};
 
 function subtract(a, b) {
     let c = a - b;
     return c;
-}
+};
 
 function multiply(a, b) {
     let c = a * b;
     return c;
-}
+};
 
 function divide(a, b) {
     let c = a / b;
     return c;
-}
+};
